c6ff56b3fbb213e4b2802fa77cc25ed317399ca5
JoseLucas1303 <74744598+JoseLucas1303@users.noreply.github.com>
2023-06-20 21:34:42 -0300

    Added Source Code

---
+++ ArticPolar.Dev.PDF2PNG/PDF2PNG.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Ghostscript.NET;
+using Ghostscript.NET.Rasterizer;
+using System.Drawing;
+using System.IO;
+namespace ArticPolar.Dev.PDF2PNG
+{
+    public class PDF2PNG
+    {
+        public void ConvertPdfToPng(string pdfFilePath, string outputDirectory)
+        {
+            if (!Directory.Exists(outputDirectory))
+            {
+                Directory.CreateDirectory(outputDirectory);
+            }
+            using (var rasterizer = new GhostscriptRasterizer())
+            {
+                rasterizer.Open(pdfFilePath);
+                for (int pageNumber = 1; pageNumber <= rasterizer.PageCount; pageNumber++)
+                {
+                    using (var image = rasterizer.GetPage(96, pageNumber))
+                    {
+                        var outputFilePath = Path.Combine(outputDirectory, "Page_" + pageNumber + ".png");
+                        image.Save(outputFilePath, System.Drawing.Imaging.ImageFormat.Png);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ PDFPNG_TestConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArticPolar.Dev.PDF2PNG;
+
+namespace PDFPNG_TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PDF2PNG converter = new PDF2PNG();
+            converter.ConvertPdfToPng(args[0], args[1]);
+            Console.WriteLine("The PDF: " + args[0] + " is converted in Folder: " + args[1]);
+        }
+    }
+}
